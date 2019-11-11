8018ba4c31a50b7784eadad58321d510b072062a
tompannet <55949240+tompannet@users.noreply.github.com>
2019-11-11 14:15:38 +0100

    Create dolwak.js

---
+++ .gitignore/dolwak.js
@@ -0,0 +1,144 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var bot = new Discord.Client();
+var nombreAleatoire = Math.round(Math.random()*1);
+var reponse;
+var Ran = Math.floor(Math.random() * 10);
+
+client.login("NjI3ODM0ODIyNTI5OTA4NzU1.XZCh4Q.jG5xd-uzQbSOUZBk8sL4Ngf7j7M");
+
+var prefix = ("dolwak");
+
+client.on("message", (message) => {
+
+    if(message.content === prefix + " véhicule"){      
+        message.channel.send("C'est pas une voiture Alice, **C'EST UN BUS !!**")
+        }
+    
+    if(message.content === prefix + " sang")
+    message.channel.send("Dans la famille Meillant, écarlate doit être ton sang !")
+    
+    if(message.content === prefix + " beauté")
+    message.channel.send("Kiniou")
+    
+    if(message.content === prefix + " M2N")
+    message.channel.send("mAiS vOuS ÊtEs GrEtA ThUnBeRg ?")
+
+    if(message.content === prefix + " secte")
+    message.channel.send("La Fondation de la théorie de la relativité généraaaaale ! :boom:")
+
+    if(message.content === prefix + " danse")
+    message.channel.send("POMMES DE TERRE - OU AH ! - POMMES DE TERRE - OU AH - POMMES DE TERRE - OU AH !")
+
+    if(message.content === prefix + " orga")
+    message.channel.send("**MERCI TONY** :heart_eyes:")
+
+    if(message.content === prefix + " mortimer")
+    message.channel.send("Elaine ! Did you hear ? Did you understand ? **I'm a bastard !**")
+
+    if(message.content === prefix + " upr")
+    message.channel.send("**PENSEZ PRINTEMPS MES AMIS !**")
+
+    if(message.content === prefix + " cp")
+    message.channel.send("Typhlosioooon !")
+
+    if(message.content === prefix + " teddy")
+    message.channel.send("***CHAAAARGE ! CHAAARGE THE BLOCK HOOOOUSE !***")
+
+    if(message.content === prefix + " latin")
+    message.channel.send("Mihi placet fututio :smirk:")
+
+    if(message.content === prefix + " who")
+    message.channel.send("ARE YOU ED SHEERAN ? I'M SURE IT'S ED SHEERAN, EVERYONE TALKS ABOUT ED SHEERAN")
+
+    if(message.content === prefix + " sexy")
+    message.channel.send("T'es sexy quand tu te concentres ! :smirk:")
+
+    if(message.content === "Et oui")
+    message.channel.send("Ah bon ?")
+
+    if(message.content === "Bah ouais mec")
+    message.channel.send("Au temps pour moi !")
+
+    if(message.content === "Sans rancune ^^")
+    message.channel.send("Ouf !")
+
+    if(message.content === ":smirk:")
+    message.channel.send(":eyes:")
+
+    if(message.content === "ça va ?")
+    message.channel.send("Personnellement je suis un robot =/")
+
+    if (message.content === "connard")
+    message.channel.send("EXCUSE TOI !")
+
+    if(message.content === "pardon")
+    message.channel.send("Pas très sincère tout ça...")
+
+    if(message.content === "Nomi ?")
+    message.channel.send("Nomi SAV bonjour ! Que puis-je faire pour vous ?")
+
+    if(message.content === "Merde")
+    message.channel.send("TOUT DOUX !")
+
+    if(message.content === "merde")
+    message.channel.send("TOUT DOUX !")
+
+    if(message.content === "Ban ?")
+    message.channel.send("Tu as vu ce camion **rouge** ?")
+
+    if(message.content === "Albane ?")
+    message.channel.send("Besoin d'une pute ?")
+
+    if(message.content === "Arnaud")
+    message.channel.send(":smirk: :smirk: :smirk:")
+
+    if(message.content === "arnaud")
+    message.channel.send(":smirk: :smirk: :smirk:")
+
+    if(message.content === "Un mouton")
+    message.channel.send("Beeeh ! C'est bête un mouton !")
+
+    if(message.content === "Deux moutons")
+    message.channel.send("Beeeh beeeh ! C'est bêbête deux moutons !")
+
+    if(message.content === "a")
+    message.channel.send(":hibiscus:") ;
+
+    if(message.content === "mdr")
+    message.channel.send(":joy: :joy: :joy: :joy: :joy: :joy: :joy: :joy: :joy: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :rofl: :rofl: :rofl: :rofl: :rofl: :rofl: :rofl: :rofl::joy: :joy: :joy: :joy: :joy: :joy: :joy: :joy: :joy: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :rofl: :rofl: :rofl: :rofl: :rofl: :rofl: :rofl: :rofl::joy: :joy: :joy: :joy: :joy: :joy: :joy: :joy: :joy: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :rofl: :rofl: :rofl: :rofl: :rofl: :rofl: :rofl: :rofl::joy: :joy: :joy: :joy: :joy: :joy: :joy: :joy: :joy: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :rofl: :rofl: :rofl: :rofl: :rofl: :rofl: :rofl: :rofl::joy: :joy: :joy: :joy: :joy: :joy: :joy: :joy: :joy: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :rofl: :rofl: :rofl: :rofl: :rofl: :rofl: :rofl: :rofl::joy: :joy: :joy: :joy: :joy: :joy: :joy: :joy: :joy: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :laughing: :rofl: :rofl: :rofl: :rofl: :rofl: :rofl: :rofl: :rofl:")
+
+    if(message.content === "Bonjour !")
+    message.channel.send("Bonsoir.)")
+
+    if(message.content === "non")
+    message.channel.send("krkrkrkr")
+
+    if(message.content === "oui")
+    message.channel.send(":pensive:")
+
+    if(message.content === "qui ?")
+    message.channel.send("MOI !")
+
+    if(message.content === "Qui ?")
+    message.channel.send("MOI !")
+
+    if(message.content === "LA RÉPUBLIQUE, C'EST MOI !!")
+    message.channel.send("Je suis un homme en **COLÈRE**.")
+
+    if(message.content === "///b")
+    message.channel.send(":hibiscus: **Bienvenue sur Doliprane Wakanda** :hibiscus:")   
+    
+    if(message.content === "///c")
+    message.channel.send("Ce territoire est une terre vierge - comme Zélie - c'est donc un territoire **propice à l'imagination !** Tout est possible dans ce pays magique où le ciel est gris et l'herbe morte. **Alors amusez vous bien sur Doliprane Wakanda !** :tada:")
+    
+    if(message.content === "///d")
+    message.channel.send("*Un problème ? N'hésite pas à contacter la Milice de la Vie Privée pour toute requête*  :wink:")
+
+    if(message.content === prefix + " help")
+    message.channel.send("**Besoin d'aide ?** :arrow_right: Voici des commandes qui peuvent t'être utiles :wink: // - dolwak véhicule - dolwak sang - dolwak beauté    - dolwak M2N    - dolwak secte    - dolwak as    - dolwak orga    - dolwak mortimer    - dolwak upr    - dolwak cp     - dolwak teddy    - dolwak latin    - dolwak episode    - dolwak sexy // *Si tu as d'autres idées de dolwak amusants, parles-en à la Milice de la Vie Privée*")
+
+    if(message.content === "Ok Lucie, où est-ce que j'ai cours ?")
+    message.channel.send("On est en semaine B :wink:")
+
+})
